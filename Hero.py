4b762457943fe0ffa1ea44c7cb4d5bcc8f78e47d
Mariya-Magdalena <60431074+megi98@users.noreply.github.com>
2020-04-06 16:22:03 +0300

    add take_mana_damage

--- Hero.py
+++ Hero.py
@@ -1,6 +1,7 @@
 from character import Character
 from weaponAndSpell import Weapon, Spell
 
+
 class Hero(Character):
 
     def __init__(self, name, title, health, mana, mana_regeneration_rate):
@@ -10,28 +11,49 @@ class Hero(Character):
         self.title = title
         self.mana_regeneration_rate = mana_regeneration_rate
 
+
     def known_as(self):
+
         return "{} the {}".format(self.name, self.title)
 
+
     def is_alive(self):
+
         return super().is_alive()
 
+
     def get_health(self):
+
         return super().get_health()
 
+
     def take_damage(self, damage_points):
+
         super().take_damage(damage_points)
 
+
+    def take_mana_damage(self, damage_points):
+
+        super().take_mana_damage(damage_points)
+
+
     def get_mana(self):
+
         return super().get_mana()
 
+
     def can_cast(self, mana_cost):
+
         return super().can_cast(mana_cost)
 
+
     def take_healing(self, healing_points):
+
         return super().take_healing(healing_points)
 
+
     def take_mana(self, mana_points = None):
+
         #increasing mana in every move
         if mana_points is None:
             mana_points = self.mana_regeneration_rate
@@ -41,11 +63,17 @@ class Hero(Character):
         if self.mana > self.start_mana:
             self.mana = self.start_mana
 
+
     def equip(self, weapon):
+
         super().equip(weapon)
 
+
     def learn(self, spell):
+
         super().learn(spell)
 
+
     def attack(self, by):
+
         return super().attack(by)
